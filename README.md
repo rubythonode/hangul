33c0d60cdd26a3f85db8805621d3c2d162cd8557
DSMGun <geon0250@gmail.com>
2017-08-13 20:27:58 +0900

    Test and Modify

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
 # 한글 음소 분리기
-한글을 초성, 중성, 종성으로 분리하여 줍니다.
+한글을 초성, 중성, 종성으로 분리하여줍니다. 또한, 초성, 중성, 종성을 조합하여 글자를 만들 수 있습니다.
+
+# Docs
+## `SeparateToRune(r rune) ([]rune, error)`
+한글 한 글자를 []rune으로 나눈다.
+## `SeparateToInt(r rune) ([]int, error)`
+한글 한 글자를 []int로 나눈다.
+## `BuildOfInt(f, s, t int) (rune, error)`
+초성, 중성, 종성을 int로 받아 한 글자로 조합하여 rune으로 반환한다.
+## `BuildOfRune(f, s, t rune) (rune, error)`
+초성, 중성, 종성을 rune으로 받아 한 글자로 조합하여 rune으로 반환한다.
+## `Choseong(n int) (rune, error)`
+n번째 초성을 rune으로 반환한다.
+## `Jungseong(n int) (rune, error)`
+n번째 중성을 rune으로 반환한다.
+## `Jongseong(n int) (rune, error)`
+n번째 종성을 rune으로 반환한다.
+## `IndexOfChoseong(r rune) (int, error)`
+초성의 인덱스를 반환한다. (없을 시 -1 반환)
+## `IndexOfJungseong(r rune) (int, error)`
+중성의 인덱스를 반환한다. (없을 시 -1 반환)
+## `IndexOfJongseong(r rune) (int, error)`
+종성의 인덱스를 반환한다. (없을 시 -1 반환)
+## `IsHangulLetter(r rune) bool`
+초성이아닌 한글인지 판단하여 bool으로 반환.
+## `IsHangul(r rune) bool`
+초성 여부와 상관없이 한글인지 판단하여 bool으로 반환.
+
+# 참고
+## 한글 표
+| Idx | 초성 | 중성 | 종성 | Idx | 초성 | 중성 | 종성 |
+|:---:|:----:|:----:|:----:|:---:|:----:|:----:|:----:|
+|  0  |  ㄱ  |  ㅏ  | 없음 |  14 |  ㅊ  |  ㅜ  |  ㄿ  |
+|  1  |  ㄲ  |  ㅐ  |  ㄱ  |  15 |  ㅋ  |  ㅝ  |  ㅀ  |
+|  2  |  ㄴ  |  ㅑ  |  ㄲ  |  16 |  ㅌ  |  ㅞ  |  ㅁ  |
+|  3  |  ㄷ  |  ㅒ  |  ㄳ  |  17 |  ㅍ  |  ㅟ  |  ㅂ  |
+|  4  |  ㄸ  |  ㅓ  |  ㄴ  |  18 |  ㅎ  |  ㅡ  |  ㅄ  |
+|  5  |  ㄹ  |  ㅔ  |  ㄵ  |  19 |   -  |  ㅢ  |  ㅅ  |
+|  6  |  ㅁ  |  ㅕ  |  ㄶ  |  20 |   -  |  ㅣ  |  ㅆ  |
+|  7  |  ㅂ  |  ㅖ  |  ㄷ  |  21 |   -  |   -  |  ㅇ  |
+|  8  |  ㅃ  |  ㅗ  |  ㄹ  |  22 |   -  |   -  |  ㅈ  |
+|  9  |  ㅅ  |  ㅠ  |  ㄺ  |  23 |   -  |   -  |  ㅊ  |
+|  10 |  ㅆ  |  ㅘ  |  ㄻ  |  24 |   -  |   -  |  ㅋ  |
+|  11 |  ㅇ  |  ㅛ  |  ㄼ  |  25 |   -  |   -  |  ㅌ  |
+|  12 |  ㅈ  |  ㅙ  |  ㄽ  |  26 |   -  |   -  |  ㅍ  |
+|  13 |  ㅉ  |  ㅘ  |  ㄾ  |  27 |   -  |   -  |  ㅎ  |
--- hangul.go
+++ hangul.go
@@ -2,17 +2,21 @@ package hangul
 
 import (
 	"fmt"
+	"sort"
 	"unicode/utf8"
 )
 
 var (
-	choseong  = [...]rune{'ㄱ', 'ㄲ', 'ㄴ', 'ㄷ', 'ㄸ', 'ㄹ', 'ㅁ', 'ㅂ', 'ㅃ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅉ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ'}
+	// 19
+	choseong = [...]rune{'ㄱ', 'ㄲ', 'ㄴ', 'ㄷ', 'ㄸ', 'ㄹ', 'ㅁ', 'ㅂ', 'ㅃ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅉ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ'}
+	// 21
 	jungseong = [...]rune{'ㅏ', 'ㅐ', 'ㅑ', 'ㅒ', 'ㅓ', 'ㅔ', 'ㅕ', 'ㅖ', 'ㅗ', 'ㅘ', 'ㅙ', 'ㅚ', 'ㅛ', 'ㅜ', 'ㅝ', 'ㅞ', 'ㅟ', 'ㅠ', 'ㅡ', 'ㅢ', 'ㅣ'}
+	// 28
 	jongseong = [...]rune{0, 'ㄱ', 'ㄲ', 'ㄳ', 'ㄴ', 'ㄵ', 'ㄶ', 'ㄷ', 'ㄹ', 'ㄺ', 'ㄻ', 'ㄼ', 'ㄽ', 'ㄾ', 'ㄿ', 'ㅀ', 'ㅁ', 'ㅂ', 'ㅄ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ'}
 )
 
 func SeparateToRune(r rune) ([]rune, error) {
-	if IsHangul(r) == true {
+	if IsHangulLetter(r) == true {
 		var sep []rune = make([]rune, 3)
 
 		sep[0] = choseong[int((((r-0xAC00)-(r-0xAC00)%28)/28)/21)]
@@ -26,7 +30,7 @@ func SeparateToRune(r rune) ([]rune, error) {
 }
 
 func SeparateToInt(r rune) ([]int, error) {
-	if IsHangul(r) == true {
+	if IsHangulLetter(r) == true {
 		var sep []int = make([]int, 3)
 
 		sep[0] = int((((r - 0xAC00) - (r-0xAC00)%28) / 28) / 21)
@@ -40,8 +44,8 @@ func SeparateToInt(r rune) ([]int, error) {
 }
 
 func BuildOfInt(f, s, t int) (rune, error) {
-	var r rune = rune(0xAC00 + 28*21*f + 28*s + t)
-	if IsHangul(r) == true {
+	var r rune = rune(44032 + 588*f + 28*s + t)
+	if IsHangulLetter(r) == true {
 		return r, nil
 	} else {
 		return 0, fmt.Errorf("'%c' is not Hangul.", r)
@@ -67,51 +71,59 @@ func BuildOfRune(f, s, t rune) (rune, error) {
 		return 0, err
 	}
 
-	var r rune = rune(0xAC00 + 28*21*iF + 28*iS + iT)
-	if IsHangul(r) == true {
-		return r, nil
+	var r rune = rune(44032 + 588*iF + 28*iS + iT)
+	return r, nil
+}
+
+func Choseong(n int) (rune, error) {
+	if n < 0 || n > len(choseong) {
+		return 0, fmt.Errorf("Index out of range: Choseong[19]")
 	} else {
-		return 0, fmt.Errorf("'%c' is not Hangul.", r)
+		return choseong[n], nil
 	}
 }
 
-func Choseong(n int) rune {
-	return choseong[n]
+func Jungseong(n int) (rune, error) {
+	if n < 0 || n > len(jungseong) {
+		return 0, fmt.Errorf("Index out of range: Jungseong[21]")
+	} else {
+		return jungseong[n], nil
+	}
 }
 
-func Jungseong(n int) rune {
-	return jungseong[n]
-}
-
-func Jongseong(n int) rune {
-	return jongseong[n]
+func Jongseong(n int) (rune, error) {
+	if n < 0 || n > len(jongseong) {
+		return 0, fmt.Errorf("Index out of range: Jongseong[28]")
+	} else {
+		return jongseong[n], nil
+	}
 }
 
 func IndexOfChoseong(r rune) (int, error) {
-	for i, v := range choseong {
-		if v == r {
-			return i, nil
-		}
+	i := sort.Search(len(choseong), func(i int) bool { return choseong[i] >= r })
+	if i < len(choseong) && choseong[i] == r {
+		return i, nil
+	} else {
+		return -1, fmt.Errorf("Choseong does not contain '%c'", r)
 	}
-	return 0, fmt.Errorf("Choseong does not contain '%c'", r)
 }
 
 func IndexOfJungseong(r rune) (int, error) {
-	for i, v := range jungseong {
-		if v == r {
-			return i, nil
-		}
+	i := sort.Search(len(jungseong), func(i int) bool { return jungseong[i] >= r })
+	if i < len(jungseong) && jungseong[i] == r {
+		return i, nil
+	} else {
+		return -1, fmt.Errorf("Jungseong does not contain '%c'", r)
 	}
-	return 0, fmt.Errorf("Jungseong does not contain '%c", r)
 }
 
 func IndexOfJongseong(r rune) (int, error) {
-	for i, v := range jongseong {
-		if v == r {
-			return i, nil
-		}
+	i := sort.Search(len(jongseong), func(i int) bool { return jongseong[i] >= r })
+	if i < len(jongseong) && jongseong[i] == r {
+		return i, nil
+	} else {
+		return -1, fmt.Errorf("Jongseong does not contain '%c'", r)
 	}
-	return 0, fmt.Errorf("Jongseong does not contain '%c'", r)
 }
 
 func IsHangulLetter(r rune) bool {
